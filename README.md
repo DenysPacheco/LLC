a56fbe2e86ee59c7f6c3f7bedd4c490281cb1401
Denys Pacheco <denysdohn@gmail.com>
2022-11-25 18:56:57 -0300

    updt(misc): updt hashcodes & add editorconfig

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Little random codes and challenges that are fun to make.
 
 Challenges are usually from [Code Wars](https://www.codewars.com/kata).
 
-![LLC](repository-open-graph-template.png 'Lovely little code')
+![LLC](repository-open-graph-template.png "Lovely little code")
 
 Made with ❤️ by [Denys](https://github.com/DenysPacheco).
--- hashcodes.py
+++ hashcodes.py
@@ -1,5 +1,9 @@
+import hashlib
 import time
 import random
+
+
+# ⚠️ I know 'random.random()' is faster and better for just 6 digits codes over 10**9 runs but I wanted to implement something based on time and changeable between calls
 
 
 def random_hash(digits: int) -> str:
@@ -27,15 +31,49 @@ def random_hash(digits: int) -> str:
     return rhash
 
 
-print(random_hash(8))
-print(random_hash(6))
-print(random_hash(4))
+# print(random_hash(8))
+# print(random_hash(6))
+# print(random_hash(4))
 
-start = time.time()
+# start = time.time()
 
-for x in range(1000000):
-    random_hash(6)
+# for x in range(1000000):
+#     random_hash(6)
 
-end = time.time()
+# end = time.time()
 
-print(f"{(end-start):.2f} secs")
+# print(f"{(end-start):.2f} secs")
+
+# 😲 Learning is the way: 'random.seed' is bad for this!
+# plus, this one is faster and safer
+
+def random_sha256(size: int) -> str:
+    """Makes a random alphanumeric sha256 hashed string.
+
+    Args:
+        size (int): size of the hash string
+
+    Returns:
+        str: alphanumeric hash string
+    """
+    # encode to the hash function
+    enc_rand = str(random.random()).encode()
+    # hash it using sha256
+    hashed = hashlib.sha256(enc_rand)
+    # digests and splits the last chars with size
+    dec_hash = hashed.hexdigest()[-size:]
+
+    return dec_hash
+
+    # compact
+    # return hashlib.sha256(str(random.random()).encode()).hexdigest()[-size:]
+
+
+# start = time.time()
+
+# for x in range(1000000):
+#     random_sha256(12)
+
+# end = time.time()
+
+# print(f"{(end-start):.2f} secs")
